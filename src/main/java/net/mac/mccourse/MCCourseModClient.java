ec9d0452f7f2d76d1fccb87fcf2b349c0e292813
GrassBlock210 <macklyn.smith.nz@gmail.com>
2024-06-10 17:55:49 +1200

    Initial commit

---
+++ src/main/java/net/mac/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package net.mac.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
